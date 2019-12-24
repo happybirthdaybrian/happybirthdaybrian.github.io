06879b5711ad5531fe6a25eda5425cf314c4d0ae
Trevor Porter <trkporter@ucdavis.edu>
2019-12-23 18:45:11 -0800

    everything

---
+++ precache-manifest.cf1165be74b0a18d5f2c8e34f1bf9dcd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26dd578e2bf0afda989066405c6a5f47",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b27d66fc7b4578e2723",
+    "url": "/static/css/main.de9f1e80.chunk.css"
+  },
+  {
+    "revision": "8d94c7513243bd476aa3",
+    "url": "/static/js/2.94d16d6d.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.94d16d6d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6b27d66fc7b4578e2723",
+    "url": "/static/js/main.bf703757.chunk.js"
+  },
+  {
+    "revision": "168f293576b4c01a9462",
+    "url": "/static/js/runtime-main.b04bf341.js"
+  }
+]);
